b8f370f79150e90765601567a52173f231a43101
YUVRAJ MANDLOI <158153735+yuvigurjar@users.noreply.github.com>
2024-07-15 17:25:14 +0530

    second commit

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+function call(){
+    var x = pompt("Full name");
+    document.getElementById("ach3").innerHTML= x;
+ }
+ function reset(){
+ 
+     document.getElementById("input1").value= "";
+     
+     document.getElementById("input2").value= "";
+     
+     document.getElementById("input3").value= "";
+ }
